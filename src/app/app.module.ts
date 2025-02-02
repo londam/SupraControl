e00678ae1498383e1abc0012571a80525b30a049
Damir Lončarić <damir.loncaric987@gmail.com>
2025-02-02 12:10:02 +0100

    login working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DxButtonModule } from 'devextreme-angular';
+import { DxButtonModule, DxTextBoxModule } from 'devextreme-angular';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, DxButtonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DxButtonModule,
+    DxTextBoxModule,
+    CommonModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = 'testniZadatak';
+  password: string = 'TestniZadatak123.';
+  errorMessage: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login() {
+    const credentials = btoa(`${this.username}:${this.password}`);
+    const headers = { Authorization: `Basic ${credentials}` };
+
+    this.http
+      .post(
+        'https://dev.supracontrol.com:8080/api/user/authenticate/',
+        {},
+        { headers, observe: 'response' }
+      )
+      .subscribe({
+        next: (response) => {
+          const token = response.headers.get('Token');
+          if (token) {
+            localStorage.setItem('token', token);
+            this.router.navigate(['/users']);
+          } else {
+            this.errorMessage = 'Neuspješna prijava. Token nije pronađen.';
+          }
+        },
+        error: () => {
+          this.errorMessage = 'Neuspješna prijava. Provjerite podatke.';
+        },
+      });
+  }
 }
